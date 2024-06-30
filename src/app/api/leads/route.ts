2bd65540b75787e72a7d48e54a71aca470d49462
Alwyn Tablatin <alwyn@contact.xyz>
2024-06-30 14:49:49 +0800

    implement example

---
+++ src/app/api/leads/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from 'next/server';
+import { z as zod } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+
+import * as db from '@/app/lib/db';
+import * as schema from '@/app/lib/schema';
+
+export const dynamic = 'force-dynamic';
+export const runtime = 'nodejs';
+export const fetchCache = 'force-no-store';
+export const revalidate = 0;
+
+export async function POST(request: Request) {
+  const contentType = await request.headers.get('content-type');
+  if (contentType !== 'application/json') {
+    return NextResponse.json(
+      { message: 'Invalid request' },
+      { status: 415 }
+    );
+  }
+  const data = await request.json();
+  let parsedData = {};
+  try {
+    parsedData = await schema.insertLeadTableSchema.parse(data);
+  } catch (error) {
+    if (error instanceof zod.ZodError) {
+      const validationError = fromZodError(error);
+      return NextResponse.json(
+        { errorList: validationError },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      { message: 'Some Server Error' },
+      { status: 500 }
+    );
+  }
+  const { email } = parsedData;
+  if (!email) {
+    return NextResponse.json(
+      { message: 'A valid email is required' },
+      { status: 400 }
+    );
+  }
+  const dbNow = await db.dbNow();
+  const leadResult = await db.addLead({ email: email });
+
+  const resultData = {
+    leadResult: leadResult,
+    dbNow: dbNow,
+  };
+  return NextResponse.json(resultData, { status: 201 });
+}
---
+++ src/app/lead-capture/form.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { useState, useRef } from 'react';
+
+export default function LeadCaptureForm() {
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
+  const formRef = useRef(null);
+
+  const handleForm = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const formData = new FormData(event.target);
+
+    const dataObject = Object.fromEntries(formData);
+
+    const jsonData = JSON.stringify(dataObject);
+
+    const options = {
+      method: 'POST', // HTTP POST
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonData,
+    };
+
+    // fetch
+    const response = await fetch('/api/leads/', options);
+    // const responseData = await response.json()
+    if (response.ok) {
+      setMessage('Thank you for joining');
+      formRef.current.reset();
+    } else {
+      setMessage('Error with your request');
+    }
+    setLoading(false);
+  };
+  const btnLabel = loading ? 'Loading' : 'Join List';
+  return (
+    <>
+      {message && <div>{message}</div>}
+      <form ref={formRef} className="space-y-3" onSubmit={handleForm}>
+        <input
+          type="email"
+          required
+          name="email"
+          placeholder="Your Email"
+        />
+        <button disabled={loading} className="btn-join" type="submit">
+          {btnLabel}
+        </button>
+      </form>
+    </>
+  );
+}
